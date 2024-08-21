32fd52c62fa6fe58d7e19347ca38539ead91ba7e
AlexhpSENATI <1510696@senati.pe>
2024-08-21 12:39:01 -0500

    consulta actual

--- SEMANA_01/trabajo.py
+++ SEMANA_01/trabajo.py
@@ -1,1 +1,10 @@
-print("EDISON EL TRISTE")
+
+usuarios = [
+    {"nombre": "Juan", "edad": 30},
+    {"nombre": "Diana", "edad": 25},
+    {"nombre": "Mario", "edad": 35}
+]
+resultado = [usuario for usuario in usuarios if usuario["edad"] > 30]
+
+for usuario in resultado:
+    print(usuario)
